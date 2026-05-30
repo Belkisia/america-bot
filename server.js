f913a4c95d617fa4f873ed2990c9adabed4e23c2
Belkisia <realfarma2011@gmail.com>
2026-05-30 10:24:14 -0300

    Update server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const db = require('./supabase');
 
 const app = express();
 app.use(express.json());
-app.use(express.static('spublic'));
+app.use(express.static('public'));
 
 const {
   ANTHROPIC_API_KEY,
