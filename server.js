68558f89c1ba45aba93b2f26897ae963c5716255
Belkisia <realfarma2011@gmail.com>
2026-05-30 13:10:41 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-// require('dotenv').config();
+require('dotenv').config({ path: '/dev/null' });
 const express = require('express');
 const axios = require('axios');
 const db = require('./supabase');
