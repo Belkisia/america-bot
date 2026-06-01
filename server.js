40b802175bba20acd92ae1141725de2288233d52
Belkisia <realfarma2011@gmail.com>
2026-06-01 18:28:41 -0300

    Update server.js

--- server.js
+++ server.js
@@ -56,7 +56,7 @@ app.post('/webhook', async function(req, res) {
 });
 app.get('/setup-zapi', async function(req, res) {
   try {
-   console.log('CLIENT-TOKEN:', process.env.ZAPI_CLIENT_TOKEN); const r = await axios.put(
+    const r = await axios.put(
       'https://api.z-api.io/instances/3F3F8C9C08E0135E5F3B6653CAD29060/token/64D60F31AD6196D01A97A0D3/update-webhook-received',
       { value: 'https://america-bot-production.up.railway.app/webhook' },
       { headers: { 'Content-Type': 'application/json', 'Client-Token': process.env.ZAPI_CLIENT_TOKEN } }
